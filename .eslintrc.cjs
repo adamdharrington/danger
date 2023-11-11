38ba18d64f6a13df57712293ae9a4dd119959191
Adam Harrington <adamdharrington@gmail.com>
2023-11-11 13:35:46 +0000

    feat: danger setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    extends: ['eslint:recommended', '@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+}
